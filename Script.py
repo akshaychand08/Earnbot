44aef1094080902b8b26e292805c625008e69acd
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2024-01-07 22:59:42 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -214,16 +214,18 @@ Step 2: Add your website and API
 
 Exp: /set_shortlink 1</code> <a href=https://publicearn.com/ref/AkshayChand10>publicearn.com</a> 4b392f8eb6ad711be589ae36 (verify & page)
 
-On Second Verify (only Group) /verify2 True</code> Off Second Verify /verify2 False</code>
+On Second Verify (only Group) /verify2 True</code>
+Off Second Verify /verify2 False</code>
+
 #Note second verification 4 Hours will happen after.
-     /set_shortlink 2</code> <a href=https://link2paisa.com/ref/AkshayChand10>link2paisa.com</a> 4b392f8eb6ad711be589ae36 (Second Verify)
+/set_shortlink 2</code> <a href=https://link2paisa.com/ref/AkshayChand10>link2paisa.com</a> 4b392f8eb6ad711be589ae36 (Second Verify)
 
 Step 3: add your how to verify & download video
 
     👇 how to add 👇
 
 Exp:  /set_tutorial 1 video link</code> (verify & page)
-      /set_tutorial 2 video link</code> (Second Verify)
+       /set_tutorial 2 video link</code> (Second Verify)
       
 Step 4: Check Your All Details 
         command /details (Work Only Group)</b>"""
